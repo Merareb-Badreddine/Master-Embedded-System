7c3ac50e4db328fd818603d200d478054b1b7e3d
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-06-03 14:28:58 +0100

    Add files via upload

---
+++ Can_Bus/main.c
@@ -0,0 +1,433 @@
+/* USER CODE BEGI
+ * N Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+CAN_HandleTypeDef hcan1;
+
+    uint8_t TX_DATA[8] ;
+	uint8_t FrameNo = 0;
+	uint8_t RX_DATA[8];
+	uint32_t RX_ID = 0;
+	uint8_t RX_DLC = 0;
+    #define ACC 1
+    #define NOACC 1
+	uint8_t Speed;
+void Can_TX(uint32_t ID , uint8_t DLC , uint8_t* Payload, uint8_t polling_EN)
+{
+
+	uint32_t pTxMailbox , Number_of_free_Tx_Mailboxes = 0;
+	CAN_TxHeaderTypeDef pHeader ;
+	pHeader.DLC = DLC ;
+	pHeader.IDE = CAN_ID_STD ;
+	pHeader.RTR = CAN_RTR_DATA ;
+	pHeader.StdId = ID ;
+
+
+	//(++) HAL_CAN_GetTxMailboxesFreeLevel() to get the number of free Tx
+	//     mailboxes.
+	Number_of_free_Tx_Mailboxes = HAL_CAN_GetTxMailboxesFreeLevel(&hcan1);
+
+	if (Number_of_free_Tx_Mailboxes)
+	{
+		//(++) HAL_CAN_AddTxMessage() to request transmission of a new
+		//message.
+		if (HAL_CAN_AddTxMessage(&hcan1 ,&pHeader ,Payload ,&pTxMailbox) != HAL_OK ) {
+			Error_Handler();
+		}
+    if (polling_EN)
+    {
+		//(++) HAL_CAN_IsTxMessagePending() to check if a message is pending
+		//    in a TX mailbox.
+
+		//wait until Mailbox is transmitted (Polling Mechanism)
+		while (HAL_CAN_IsTxMessagePending(&hcan1, pTxMailbox) ) ;
+    }
+	}
+}
+
+
+void Can_RX_Filter_init(uint16_t STD_Filter_ID,  uint16_t STD_Filter_MASK)
+{
+	//(#) Configure the reception filters using the following configuration
+	//functions:
+	//(++) HAL_CAN_ConfigFilter()
+
+	CAN_FilterTypeDef sFilterConfig ;
+	sFilterConfig.FilterActivation = CAN_FILTER_ENABLE ;
+	sFilterConfig.FilterBank = 0 ;
+	sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0 ;
+	sFilterConfig.FilterIdHigh = STD_Filter_ID << 5 ;
+	sFilterConfig.FilterIdLow = 0x0000 ;
+	sFilterConfig.FilterMaskIdHigh = STD_Filter_MASK << 5 ;
+	sFilterConfig.FilterMaskIdLow = 0x0000 ;
+
+	sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK ;
+	sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT ;
+
+	if (HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig) != HAL_OK )
+	{
+		Error_Handler();
+	}  ;
+}
+// SID ID
+// DATA FRAME
+void Can_RX(uint32_t* ID , uint8_t* DLC , uint8_t* Payload, uint8_t polling_EN)
+{
+
+	CAN_RxHeaderTypeDef pHeader ;
+
+	if (polling_EN)
+	{
+	//(#) Reception:
+	// (++) Monitor reception of message using HAL_CAN_GetRxFifoFillLevel()
+	//until at least one message is received.
+	//(++) Then get the message using HAL_CAN_GetRxMessage().
+
+	//wait
+	while(HAL_CAN_GetRxFifoFillLevel(&hcan1, CAN_FILTER_FIFO0) == 0 );
+	}
+
+	if (HAL_CAN_GetRxMessage(&hcan1, CAN_FILTER_FIFO0, &pHeader, Payload) != HAL_OK ) {
+		Error_Handler();
+	}
+	*ID = pHeader.StdId ;
+	*DLC = pHeader.DLC ;
+}
+
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+//IRQ Callbacks
+void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan)
+{
+
+}
+//------------------------------------------------------------------------------------------
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	Can_RX(&RX_ID,&RX_DLC, RX_DATA,0);
+	Speed = RX_DATA[0];
+}
+//------------------------------------------------------------------------------------------
+
+void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan)
+{
+	if ((hcan->ErrorCode & HAL_CAN_ERROR_TX_TERR0 ) == HAL_CAN_ERROR_TX_TERR0)   /*!< TxMailbox 0 transmit failure due to transmit error   */
+		{
+
+		}
+}
+//------------------------------------------------------------------------------------------
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_CAN1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_CAN1_Init();
+
+  // RX filter
+  Can_RX_Filter_init(0x3AB, 0x7FF);
+  //interrupt CAN Notification
+  if (HAL_CAN_ActivateNotification(&hcan1,( CAN_IT_TX_MAILBOX_EMPTY | CAN_IT_RX_FIFO0_MSG_PENDING )) != HAL_OK)
+  {
+	Error_Handler();
+  }
+  //Start CAN
+  if (HAL_CAN_Start(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  //RX Filter
+
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  TX_DATA[8] = ACC;
+  while (1)
+  {
+    /* USER CODE END WHILE */
+       HAL_Delay(1000);
+       Can_TX(0x030,1, TX_DATA ,0);
+       TX_DATA[0] ^= 1;
+
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief CAN1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN1_Init(void)
+{
+
+  /* USER CODE BEGIN CAN1_Init 0 */
+
+  /* USER CODE END CAN1_Init 0 */
+
+  /* USER CODE BEGIN CAN1_Init 1 */
+
+  /* USER CODE END CAN1_Init 1 */
+  hcan1.Instance = CAN1;
+  hcan1.Init.Prescaler = 1;
+  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_6TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan1.Init.TimeTriggeredMode = DISABLE;
+  hcan1.Init.AutoBusOff = DISABLE;
+  hcan1.Init.AutoWakeUp = DISABLE;
+  hcan1.Init.AutoRetransmission = DISABLE;
+  hcan1.Init.ReceiveFifoLocked = DISABLE;
+  hcan1.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN1_Init 2 */
+
+  /* USER CODE END CAN1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
