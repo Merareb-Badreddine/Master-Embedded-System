3514bd0f4ba93043e89b2d6efab69c3a87e2f67b
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-06-13 17:00:27 +0100

    Create RCC_Stm32_F103C6_Driver.h

---
+++ Driver/STM32/STM32_F103X8_Driver/RCC_Stm32_F103C6_Driver.h
@@ -0,0 +1,24 @@
+/*
+ * stm32f103x6_RCC_driver.h
+ *
+ *  Created on:
+ *      Author: 
+ */
+
+#ifndef RCC_Stm32_F103C6_Driver_H_
+#define RCC_Stm32_F103C6_Driver_H_
+
+#include "Stm32_F103X6_Mcu_Device_Header"
+#include "GPIO_Stm32_F103C6_Driver.h"
+
+#define HSE_CLK 		(uint32_t)16000000
+#define HSI_RC_CLK 		(uint32_t)8000000
+
+uint32_t MCAL_RCC_GetSYS_CLKFreq(void);
+
+uint32_t MCAL_RCC_GetHCLKFreq(void);
+
+uint32_t MCAL_RCC_GetPCLK1Freq(void);
+uint32_t MCAL_RCC_GetPCLK2Freq(void);
+
+#endif /* RCC_Stm32_F103C6_Driver_H_ */
