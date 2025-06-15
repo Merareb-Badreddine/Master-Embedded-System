7615fd36d73343565f3229f383f3e7bc8b5cef34
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-06-15 14:52:11 +0100

    Update and rename FPEC_Driver to FPEC_STM32_L47bRGTbo_Driver.c

---
+++ FOTA/FPEC_STM32_L47bRGTbo_Driver.c
@@ -0,0 +1,85 @@
+/*
+ * FPEC_STM32_L47bRGTbo_Driver.c
+ *
+ *  Created on:
+ *      Author: badreddine.merareb
+ */
+#include "FPEC_STM_L47bRGTbo_Driver.h"
+
+
+
+void MCAL_FPEC_Write(uint32_t Address, uint32_t *Data, uint32_t Data_Length)
+{
+	uint32_t Local_Counter = 0;
+
+		if (FLASH->CR & FLASH_CR_LOCK)
+		    {
+		        FLASH->KEYR = FLASH_KEY1;
+		        FLASH->KEYR = FLASH_KEY2;
+		    }
+
+	 for (Local_Counter = 0; Local_Counter < Data_Length; Local_Counter += 2)
+	  {
+
+		 while (FLASH_R->SR & FLASH_SR_BSY1);
+		 FLASH_R->CR |= FLASH_CR_PG;
+
+		 uint64_t data64 = ((uint64_t)Data[Local_Counter + 1] << 32) | Data[Local_Counter];
+		 *(volatile uint64_t *)(Address) = data64;
+
+		  while (FLASH->SR & FLASH_SR_BSY1);
+		  FLASH_R->CR &= ~FLASH_CR_PG;;
+		  if (FLASH->SR & FLASH_SR_EOP)
+	        {
+	            FLASH->SR |= FLASH_SR_EOP;
+	        }
+
+	        Address += 8;
+	    }
+
+}
+
+void MCAL_FPEC_ErasPages(uint8_t Page_Numbre, uint8_t N_OF_Page)
+{
+
+ uint32_t Local_Counter = 0;
+  if (FLASH->CR & FLASH_CR_LOCK)
+ 		    {
+ 		        FLASH->KEYR = FLASH_KEY1;
+ 		        FLASH->KEYR = FLASH_KEY2;
+ 		    }
+  for (Local_Counter = 0; Local_Counter < N_OF_Page; Local_Counter++)
+  	  {
+
+		 while (FLASH_R->SR & FLASH_SR_BSY1);
+
+		 FLASH->CR |= FLASH_CR_PER;
+
+		 FLASH->CR &= ~(0xFF << FLASH_PAGE_NUMBER_POS);
+		 FLASH->CR |= (Page_Numbre << FLASH_PAGE_NUMBER_POS);
+
+		 FLASH->CR |= FLASH_CR_STRT;
+
+
+		while (FLASH->SR & FLASH_SR_BSY1);
+
+
+		    if (FLASH->SR & FLASH_SR_EOP)
+		    {
+		        FLASH->SR |= FLASH_SR_EOP;
+		    }
+
+
+		    FLASH->CR &= ~FLASH_CR_PER;
+
+  	  }
+  Page_Numbre++;
+}
+
+uint8_t MCAL_FPEC_Get_ReadProtectionLevel(void)
+{
+
+	 uint8_t rdp_level = (uint8_t)(FLASH->OPTR & 0xFF);
+
+	    return rdp_level;
+}
