77e6a422c4afdb716f3a576328835452d267bfbe
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-06-15 15:39:37 +0100

    Update FPEC_STM32_L47bRGTbo_Driver.c

--- FOTA/FPEC_DRIVER/FPEC_STM32_L47bRGTbo_Driver.c
+++ FOTA/FPEC_DRIVER/FPEC_STM32_L47bRGTbo_Driver.c
@@ -12,10 +12,10 @@ void MCAL_FPEC_Write(uint32_t Address, uint32_t *Data, uint32_t Data_Length)
 {
 	uint32_t Local_Counter = 0;
 
-		if (FLASH->CR & FLASH_CR_LOCK)
+		if (FLASH_R->CR & FLASH_CR_LOCK)
 		    {
-		        FLASH->KEYR = FLASH_KEY1;
-		        FLASH->KEYR = FLASH_KEY2;
+			FLASH_R->KEYR = FLASH_KEY1;
+			FLASH_R->KEYR = FLASH_KEY2;
 		    }
 
 	 for (Local_Counter = 0; Local_Counter < Data_Length; Local_Counter += 2)
@@ -27,11 +27,11 @@ void MCAL_FPEC_Write(uint32_t Address, uint32_t *Data, uint32_t Data_Length)
 		 uint64_t data64 = ((uint64_t)Data[Local_Counter + 1] << 32) | Data[Local_Counter];
 		 *(volatile uint64_t *)(Address) = data64;
 
-		  while (FLASH->SR & FLASH_SR_BSY1);
+		  while (FLASH_R->SR & FLASH_SR_BSY1);
 		  FLASH_R->CR &= ~FLASH_CR_PG;;
-		  if (FLASH->SR & FLASH_SR_EOP)
+		  if (FLASH_R->SR & FLASH_SR_EOP)
 	        {
-	            FLASH->SR |= FLASH_SR_EOP;
+			  FLASH_R->SR |= FLASH_SR_EOP;
 	        }
 
 	        Address += 8;
@@ -43,34 +43,34 @@ void MCAL_FPEC_ErasPages(uint8_t Page_Numbre, uint8_t N_OF_Page)
 {
 
  uint32_t Local_Counter = 0;
-  if (FLASH->CR & FLASH_CR_LOCK)
+  if (FLASH_R->CR & FLASH_CR_LOCK)
  		    {
- 		        FLASH->KEYR = FLASH_KEY1;
- 		        FLASH->KEYR = FLASH_KEY2;
+	  FLASH_R->KEYR = FLASH_KEY1;
+	  FLASH_R->KEYR = FLASH_KEY2;
  		    }
   for (Local_Counter = 0; Local_Counter < N_OF_Page; Local_Counter++)
   	  {
 
 		 while (FLASH_R->SR & FLASH_SR_BSY1);
 
-		 FLASH->CR |= FLASH_CR_PER;
+		 FLASH_R->CR |= FLASH_CR_PER;
 
-		 FLASH->CR &= ~(0xFF << FLASH_PAGE_NUMBER_POS);
-		 FLASH->CR |= (Page_Numbre << FLASH_PAGE_NUMBER_POS);
+		 FLASH_R->CR &= ~(0xFF << FLASH_PAGE_NUMBER_POS);
+		 FLASH_R->CR |= (Page_Numbre << FLASH_PAGE_NUMBER_POS);
 
-		 FLASH->CR |= FLASH_CR_STRT;
-
-
-		while (FLASH->SR & FLASH_SR_BSY1);
+		 FLASH_R->CR |= FLASH_CR_STRT;
 
 
-		    if (FLASH->SR & FLASH_SR_EOP)
+		while (FLASH_R->SR & FLASH_SR_BSY1);
+
+
+		    if (FLASH_R->SR & FLASH_SR_EOP)
 		    {
-		        FLASH->SR |= FLASH_SR_EOP;
+		    	FLASH_R->SR |= FLASH_SR_EOP;
 		    }
 
 
-		    FLASH->CR &= ~FLASH_CR_PER;
+		    FLASH_R->CR &= ~FLASH_CR_PER;
 
   	  }
   Page_Numbre++;
