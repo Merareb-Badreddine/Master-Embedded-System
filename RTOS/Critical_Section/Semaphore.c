be1f17be0c44cb2aa41adf47509feb5f8dde356d
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-06-24 11:24:15 +0100

    Update Semaphore.c

--- RTOS/Critical_Section/Semaphore.c
+++ RTOS/Critical_Section/Semaphore.c
@@ -1,12 +1,12 @@
 /*
-*  BootLoader.h
+*  Semaphore.c
 *
 *
 *Created on : 
 * Author : Merareb.Badreddine
 */
 
-
+volatile uint32_t SharedCounter = 0;
 
 typedef struct {
     volatile uint8_t flag; 
@@ -28,4 +28,25 @@ void Semaphore_Wait(Semaphore_t *sem)
 void Semaphore_Signal(Semaphore_t *sem)
 {
     sem->flag = 0;  
+}
+
+
+
+void Task1(void)
+{
+    Semaphore_Wait(&sem);   
+
+    SharedCounter *= 2;
+    
+    Semaphore_Signal(&sem); 
+}
+
+// الدالة الثانية التي تستخدم المتغير المشترك
+void Task2(void)
+{
+    Semaphore_Wait(&sem);   
+
+    SharedCounter *= 2;
+
+    Semaphore_Signal(&sem); 
 }
