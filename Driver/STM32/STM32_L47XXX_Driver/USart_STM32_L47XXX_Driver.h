91120673924d8aae9d067ad3d1b6619f6fa9ace1
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-06-25 11:09:46 +0100

    Update USart_STM32_L47XXX_Driver.h

--- Driver/STM32/STM32_L47XXX_Driver/USart_STM32_L47XXX_Driver.h
+++ Driver/STM32/STM32_L47XXX_Driver/USart_STM32_L47XXX_Driver.h
@@ -1,4 +1,4 @@
-/*
+*
  * USart_STM32_L47bRGTb0_Driver.h
  *
  *  Created on:
@@ -9,8 +9,8 @@
 #define USart_STM32_L47BRGTB0_DRIVER_H_
 
 #include <STM32_L47bRGTb0_Device_Header.h>
-#include "GPIO_STM32_L47bRGTb0_Driver.h"
-
+#include <GPIO_STM32_L47bRGTb0_Driver.h>
+#include <RCC_STM32_L47bRGTb0_Driver.h>
 typedef struct
 {
 	uint8_t  USart_Mode;
@@ -38,6 +38,17 @@ typedef struct
 #define USART_Mode_RX        ((uint32_t)(1<<2))
 #define USART_Mode_TX        ((uint32_t)(1<<3))
 #define USART_Mode_TX_RX     ((uint32_t)(USART_Mode_RX | USART_Mode_TX))
+
+
+#define USART_BAUDRATE_9600        9600U
+#define USART_BAUDRATE_19200       19200U
+#define USART_BAUDRATE_38400       38400U
+#define USART_BAUDRATE_57600       57600U
+#define USART_BAUDRATE_115200      115200U
+#define USART_BAUDRATE_230400      230400U
+#define USART_BAUDRATE_460800      460800U
+#define USART_BAUDRATE_921600      921600U
+#define USART_BAUDRATE_1000000     1000000U
 
 
 #define USART_Payload_Length_7B  ((uint32_t)(1 << 28))
@@ -68,7 +79,10 @@ typedef struct
 #define USART_IRQ_Enable_RXNE  ((uint32_t)(1<<5))
 
 
-
+#define USART_HwFlowCtl_NONE       0x00000000U
+#define USART_HwFlowCtl_RTS        0x00000001U
+#define USART_HwFlowCtl_CTS        0x00000002U
+#define USART_HwFlowCtl_RTS_CTS    0x00000003U
 
 typedef enum
 {
@@ -78,21 +92,21 @@ typedef enum
 
 
 
-#define USARTDIV(_PCLK_, _BAUD_)                     (uint32_t) (_PCLK_/(16 * _BAUD_)
-#define USARTDIV_MUL100(_PCLK_, _BAUD_)              (uint32_t) ((25 * _PCLK_ ) /(4 * _BAUD_))
-#define Mantissa_MUL100(_PCLK_, _BAUD_)              (uint32_t) (USARTDIV(_PCLK_, _BAUD_) *100)
-#define Mantissa(_PCLK_, _BAUD_)                     (uint32_t) (USARTDIV(_PCLK_, _BAUD_))
-#define DIV_Fraction(_PCLK_, _BAUD_)                 (uint32_t) (((USARTDIV_MUL100(_PCLK_, _BAUD_) -  Mantissa_MUL100(_PCLK_, _BAUD_)) *16) /100)
-#define USART_BRR_Register(_PCLK_,_BAUD_)            (( Mantissa (_PCLK_, _BAUD_)) << 4 )| DIV_Fraction(_PCLK_, _BAUD_)) & 0xF )
+#define USARTDIV(_PCLK_, _BAUD_)                     ((uint32_t)((_PCLK_)/(16 * (_BAUD_))))
+#define USARTDIV_MUL100(_PCLK_, _BAUD_)              ((uint32_t)((25 * (_PCLK_)) /(4 * (_BAUD_))))
+#define Mantissa_MUL100(_PCLK_, _BAUD_)              ((uint32_t)(USARTDIV(_PCLK_, _BAUD_) * 100))
+#define Mantissa(_PCLK_, _BAUD_)                     ((uint32_t)(USARTDIV(_PCLK_, _BAUD_)))
+#define DIV_Fraction(_PCLK_, _BAUD_)                 ((uint32_t)(((USARTDIV_MUL100(_PCLK_, _BAUD_) - Mantissa_MUL100(_PCLK_, _BAUD_)) * 16) / 100))
+#define USART_BRR_Register(_PCLK_, _BAUD_)           (((Mantissa(_PCLK_, _BAUD_) << 4) | (DIV_Fraction(_PCLK_, _BAUD_) & 0x0F)))
 
 
 
-void MCAL_USart_Init(USart_TypeDef * USartx , USARt_Config_t * USart_Config );
-void MCAL_USart_DeInit(USart_TypeDef * USartx);
-void MCAL_USart_GPIO_SET_PINs(USart_TypeDef * USartx);
-void MCAL_Usart_SendData(USart_TypeDef * USartx,uint16_t *pTxBuffer,Polling_Mechanism PollingEN );
-void MCAL_Usart_ReceiveData(USart_TypeDef * USartx,uint16_t *pRxBuffer,Polling_Mechanism PollingEN);
-void MCAL_USart_Wait_TC(USart_TypeDef * USartx);
+void MCAL_USart_Init(USART_TypeDef* USartx , USARt_Config_t * USart_Config );
+void MCAL_USart_DeInit(USART_TypeDef* USartx);
+void MCAL_UART_GPIO_Set_Pins(USART_TypeDef * USARTx);
+void MCAL_Usart_SendData(USART_TypeDef* USartx,uint16_t *pTxBuffer,Polling_Mechanism PollingEN );
+void MCAL_USART_ReceiveData(USART_TypeDef* USartx,uint16_t *pRxBuffer,Polling_Mechanism PollingEN);
+void MCAL_UART_WAIT_TC(USART_TypeDef* USARTx);
 
 
 #endif /*USart_STM32_L47BRGTB0_DRIVER_H_ */
