d51538ecda0bc3ed371a55faae566d1ea08724fc
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-07-17 10:56:49 +0100

    Add files via upload

---
+++ Driver/STM32/STM32_L47XXX_Driver/Driver_GPIO_STM3247bRGTb0/Debug/MCAL/GPIO_STM32_STM32_L47XXX_Driver.d
@@ -0,0 +1,8 @@
+MCAL/GPIO_STM32_STM32_L47XXX_Driver.o: \
+ ../MCAL/GPIO_STM32_STM32_L47XXX_Driver.c \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/GPIO_STM32_STM32_L47XXX_Driver.h \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/STM32_STM32_L47XXX_Device_Header.h \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/Platform_Types.h
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/GPIO_STM32_STM32_L47XXX_Driver.h:
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/STM32_STM32_L47XXX_Device_Header.h:
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/Platform_Types.h:
---
+++ Driver/STM32/STM32_L47XXX_Driver/Driver_GPIO_STM3247bRGTb0/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/GPIO_STM32_STM32_L47XXX_Driver.h \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/STM32_STM32_L47XXX_Device_Header.h \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/Platform_Types.h
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/GPIO_STM32_STM32_L47XXX_Driver.h:
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/STM32_STM32_L47XXX_Device_Header.h:
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/Platform_Types.h:
