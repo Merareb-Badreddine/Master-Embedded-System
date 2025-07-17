05a26ccc6ef31e9446fa9b5279c24702597d0d86
Merareb Badreddine <merarebbadreddine@gmail.com>
2025-07-17 10:36:10 +0100

    Add files via upload

---
+++ Driver/STM32/STM32_L47XXX_Driver/Driver_GPIO_STM3247bRGTb0/Debug/MCAL/GPIO_STM32_L47bRGTb0_Driver.d
@@ -0,0 +1,7 @@
+MCAL/GPIO_STM32_L47bRGTb0_Driver.o: ../MCAL/GPIO_STM32_L47bRGTb0_Driver.c \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/GPIO_STM32_L47bRGTb0_Driver.h \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/STM32_L47bRGTb0_Device_Header.h \
+ C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/Platform_Types.h
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/GPIO_STM32_L47bRGTb0_Driver.h:
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/STM32_L47bRGTb0_Device_Header.h:
+C:/Users/badreddine.merareb/STM32CubeIDE/workspace_1.19.0/STM32_47bRGTb0/Driver_GPIO_STM3247bRGTb0/MCAL/inc/Platform_Types.h:
